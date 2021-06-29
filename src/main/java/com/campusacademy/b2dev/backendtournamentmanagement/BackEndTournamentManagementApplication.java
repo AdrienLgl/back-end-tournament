9f44160775e73f6443aa187be73b65981af9542a
Adrien <adrien.laigle35@gmail.com>
2021-06-29 10:16:32 +0200

    project init

---
+++ src/main/java/com/campusacademy/b2dev/backendtournamentmanagement/BackEndTournamentManagementApplication.java
@@ -0,0 +1,13 @@
+package com.campusacademy.b2dev.backendtournamentmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackEndTournamentManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackEndTournamentManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/campusacademy/b2dev/backendtournamentmanagement/BackEndTournamentManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.campusacademy.b2dev.backendtournamentmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndTournamentManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
